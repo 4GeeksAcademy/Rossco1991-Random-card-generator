b4150906c73fe0fda2ae4674e2e29111365aa2a2
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-06-24 00:57:04 +0000

    Created function unable to get function to display to console

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const deckOfCards = () => {
+    const cardFace = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    const suitOpt = ["♦", "♥", "♠", "♣"];
+    const colorOfCard = ["red", "black"];
+    const cards = [];
+
+    for (let a = 0; a < cardFace.length; a++) {
+      for (let b = 0; b < suitOpt.length; b++) {
+        const faces = cardFace[a];
+        const suit = suitOpt[b];
+        cards.push({ faces, suit });
+      }
+    }
+    return cards;
+  };
 };
+let test = deckOfCards();
+console.log(test);
